8f70ab659c1411c733e833abdd41a1635f35e715
rollershutter at github <claphands@gmx.de>
2015-10-28 20:41:32 +0100

    initial commit of helper-files

---
+++ fgfs_ext_get_airpaths.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+#
+# bash script to show external aircrafts (start-name and description)
+#
+# fgfs_ext_get_airpaths.sh
+#  ver: 0003 gf
+#
+# idea is to source this and get the array "AIRPATHS" in the parent script
+#
+# author: rollershutter@github.com
+#		  ('rollershutter' at flightgear-forums
+#			forum.flightgear-de.net
+#			forum.flightgear.org)
+##
+
+
+########################################################################
+## variables ###########################################################
+#OPT_AIRPATH_ROOT="$HOME/_opt_progs"
+
+## you could enter your external paths here in "AIRPATHS": #############
+#AIRPATHS=("$OPT_AIRPATH_ROOT/fg_aircraft_svn" \
+#			"$OPT_AIRPATH_ROOT/fg_air_3_0" \
+#			"$OPT_AIRPATH_ROOT/theomega" \
+#			"$OPT_AIRPATH_ROOT/buckaroo")
+
+TEXT_CP="einen Pfad mit Flightgear Flug-/Fahrzeugen auswählen!" ## "choose airpath"
+TXT_SHORT_CP="Pfad auswählen" ## "choose airpath"
+TXT_MORE="einen weiteren $TXT_SHORT_CP?" ## "choose another path?"
+#TXT_EXIT=" TASTE drücken zum Schliessen!" ## "press KEY to exit"
+TXT_ADD_FG_AIR="FG_ROOT/Aircraft hinzufügen?"
+
+FG_AIRPATH="/usr/share/games/flightgear/Aircraft"
+
+
+## function: ###########################################################
+user_select_path() {
+#	##test zenity last selected
+	if [ ${#AIRPATHS[@]} -ne 0 ]; then
+		## for zenity, to preselect last used/selected path
+		local XT_LOC_ARG="--filename=${AIRPATHS[$((${#AIRPATHS[@]} - 1))]}" ## "--filename=$(dirname "${AIRPATHS[$((${#AIRPATHS[@]} - 1))]}")"
+#	else
+#		echo "## no airpath yet, first run"
+	fi
+	#echo "[$XT_LOC_DIR]"
+
+	#if [ $(check_command_v "zenity") -eq 0 ]; then
+	if [ "$USER_IFACE" == "Zenity" ]; then
+		##test zenity last selected (XT_LOC_ARG)
+		USR_ENTRY=($(zenity --file-selection \
+					$XT_LOC_ARG \
+					--directory \
+					--text "$TEXT_CP" \
+					--title "$TXT_SHORT_CP"))
+	else
+		printf '%s\n' "## $TEXT_CP (hit RETURN to confirm)"
+		read USR_ENTRY
+	fi
+}
+
+check_command_v() {
+	command -v "$1" >/dev/null 2>&1 \
+		&& printf 0 \
+		|| printf 1
+}
+
+
+## main: ###############################################################
+#USER_IFACE="Console" ## "Zenity" 										## hm, silent mode (-s) is needed for compatibility with fgfs-start-script, then it is a mess with user-prompt-description texts...
+########################################################################
+
+## determine quit mode
+if [ "$1" != "-s" ]; then
+	QUIET_MODE="on"
+else
+	QUIET_MODE=""
+fi
+
+## check if zenity is installed?
+if [ $(check_command_v "zenity") -eq 0 ]; then
+	USER_IFACE="Zenity"
+	## due to gtk errors
+	export NO_AT_BRIDGE=1
+else
+	printf '%s\n' "Zenity is not installed, might install with:" " apt-get install zenity"
+	USER_IFACE="Console"
+fi
+#USER_IFACE="Console"													## testing
+
+#C_PWD=$(pwd)
+
+
+######
+if [ -n "$2" ]; then
+	AIRPATHS=("$2") ##AIRPATHS=() ##("$FG_AIRPATH") ## AIRPATHS=()
+else
+	AIRPATHS=()
+fi
+
+##
+## get user input paths loop
+if [ "${#AIRPATHS[@]}" -eq 0 ]; then
+	while true; do
+		user_select_path
+		if [ -d "$USR_ENTRY" ]; then
+			AIRPATHS[${#AIRPATHS[@]}]="$USR_ENTRY" ##"$(user_select_path)"
+			#echo "[${AIRPATHS[$((${#AIRPATHS[@]} - 1))]}]"
+			[ -n "$QUIET_MODE" ] && echo "## [${AIRPATHS[$((${#AIRPATHS[@]} - 1))]}]"
+			#printf '%s' "main: checking command returns: $(command -v "zenity" >/dev/null 2>&1)"
+		fi
+
+		#if [ $(check_command_v "zenity") -eq 0 ]; then
+		if [ "$USER_IFACE" == "Zenity" ]; then
+			zenity --question --text "$TXT_MORE" --title "$TXT_MORE" \
+				|| break
+		else
+			printf '## %s' "$TXT_MORE (y/KEY) - ja/nein"
+			read -sn1 ps_m && echo " $ps_m";
+			[ "$ps_m" != "y" ] \
+				&& break
+		fi
+	done
+fi
+
+if [ -n "$QUIET_MODE" ]; then
+	if [ "$USER_IFACE" == "Zenity" ]; then
+		zenity --question --text "$TXT_ADD_FG_AIR" --title "$TXT_ADD_FG_AIR" \
+			&& AIRPATHS=("$FG_AIRPATH" "${AIRPATHS[@]}")
+			##|| break
+	else
+		printf '## %s' "$TXT_ADD_FG_AIR (y/KEY) - ja/nein"
+		read -sn1 ps_m && echo " $ps_m"
+		[ "$ps_m" == "y" ] \
+			&& AIRPATHS=("$FG_AIRPATH" "${AIRPATHS[@]}")
+	fi
+fi
+
+
+######
+##
+#[ -n "$QUIET_MODE" ] \
+#	&& { printf '## maximise me, then hit any KEY:' \
+#		&& read -sn1 go_on \
+#			&& echo; } 
+
+
+######
+## debug
+#[ "$USER_IFACE" != "Zenity" ] && printf '## %s\n' "ps_m: $ps_m" ## "go_on: $go_on"
+
+#printf '## %s\n' "last USR_ENTRY: [${USR_ENTRY}]"
+#printf '## %s\n' "for testing, debug the collected inputs:" "[${AIRPATHS[@]}]"
+
+## this is the NEW usr-input-script-module, so exit (this could be sourced to use the internal variables outside in the sourcing instance...)
+## when sourced, this exit exits the parent...
+#exit 0
---
+++ fgfs_ext_print_air.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+#
+# bash script to show external aircrafts (start-name and description)
+#
+# fgfs_ext_print_air.sh
+#  ver: 0003 ge
+#
+# author: rollershutter@github.com
+#		  ('rollershutter' at flightgear-forums
+#			forum.flightgear-de.net
+#			forum.flightgear.org)
+##
+
+
+########################################################################
+## variables ###########################################################
+#OPT_AIRPATH_ROOT="$HOME/_opt_progs"
+
+## you could enter your external paths here in "AIRPATHS": #############
+#AIRPATHS=("$OPT_AIRPATH_ROOT/fg_aircraft_svn" \
+#			"$OPT_AIRPATH_ROOT/fg_air_3_0" \
+#			"$OPT_AIRPATH_ROOT/theomega" \
+#			"$OPT_AIRPATH_ROOT/buckaroo")
+
+TARGET="-set.xml"
+printf -v PAD '%0.1s' "#"{1..74}
+
+
+## function: ###########################################################
+check_valid_aircrafts_path() {
+	local files=("$1/"*"$TARGET");
+	if [ -f "$files" ]; then
+		for c_file in "$1/"*"$TARGET"; do
+			VALID_AIRCRAFTS[${#VALID_AIRCRAFTS[@]}]="$c_file"
+		done
+	fi
+}
+
+print_overlap() {
+	tmp_ifs="$IFS"; IFS=$'\n';
+	local c_name="$1"; local c_over=(${!c_name})
+	IFS="$tmp_ifs";
+
+	if [ ${#c_over[@]} -gt 1 ]; then
+		printf ' %s\n' "${c_over[0]}"
+		for c_e in "${c_over[@]:1}"; do
+			printf '   %-28s %s\n' " " "${c_e}"
+		done
+	else
+		printf ' %s\n' "${c_over}"
+	fi
+}
+
+
+## main: ###############################################################
+if [ "$1" == "-s" ]; then
+	SILENT_MOD="on"
+else
+	SILENT_MOD=""
+fi
+
+######
+if [ "$2" != "" ]; then
+	C_IN=("$@")
+	AIRPATHS=("${C_IN[@]:1}")
+	echo "## AIRPATHS: [${AIRPATHS[@]}]"
+else
+	echo "## no input paths, exiting..."
+	exit 1
+fi
+
+## add colours
+if [ -z "$SILENT_MOD" ]; then												## print nice
+	STR_F_ADIR='\e[1;31m%b\e[0m'
+	STR_F_AIRC='\e[1;36m%b\e[0m'
+	STR_F_SHRT='\e[0;36m%-28b\e[0m'
+else
+	STR_F_ADIR='%b'
+	STR_F_AIRC='%b'
+	STR_F_SHRT='%-28b'
+fi
+
+#[ -z "$SILENT_MOD" ] \
+#	&& printf '## maximise me, then hit any KEY:' \
+#		&& read -sn1 go_on && echo
+
+##
+for air_collection in "${AIRPATHS[@]}"; do 
+	[ -z "$SILENT_MOD" ] \
+		&& printf "\r$STR_F_ADIR\n" "$PAD" "${air_collection}:"; ##printf "\r$STR_F_ADIR\n" "$PAD" "# ${air_collection}:"; 
+
+	for p in "$air_collection"/*; do 
+		if [ -d "$p" ]; then
+			VALID_AIRCRAFTS=()
+			check_valid_aircrafts_path "$p" 
+			if [ ${#VALID_AIRCRAFTS[@]} -ne 0 ]; then
+				[ -z "$SILENT_MOD" ] \
+					&& printf " $STR_F_AIRC:\n" "$(basename "$p")"; ##printf "# $STR_F_AIRC:\n" "$(basename "$p")"; 
+
+				v_names=();
+				for e in "${VALID_AIRCRAFTS[@]}"; do 
+					e_nam="$(basename "$e")"; 
+					v_names[${#v_names[@]}]="${e_nam%%-set*}"; 
+					printf "   $STR_F_SHRT" "${v_names[$((${#v_names[@]}-1))]}"; 
+
+					if [ -z "$SILENT_MOD" ]; then
+						c_desc="$(awk -F"<|>" '$0 ~ /description*/ {print $3}' "$e")"; 
+						[ -z "${c_desc}" ] \
+							&& c_desc="n.a."; 
+						print_overlap "c_desc";
+					else
+						printf '\n'
+					fi 
+				done
+			fi
+		fi
+	done
+done
+
+exit 0
